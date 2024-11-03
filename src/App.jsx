73ed36262929b54aa9e9ff06216743b25fcaaf8d
suresh pilli <sureshpilli465@gmail.com>
2024-11-03 23:07:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import DomainSelector from './DomainSelector';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <DomainSelector />
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/DomainSelector.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import unStop from './assets/unStop.webp';
+import './App.css';
+
+const DomainSelector = () => {
+    const domainNames = [
+        'Education',
+        'Health',
+        'Transportation',
+        'Magic of AI',
+        'Technology'
+    ];
+
+    const [countdown, setCountdown] = useState(null);
+    const [selectedDomain, setSelectedDomain] = useState('');
+
+    const selectRandomDomain = () => {
+        setCountdown(5);
+        setSelectedDomain('');
+
+        const interval = setInterval(() => {
+            setCountdown((prev) => {
+                if (prev > 1) {
+                    return prev - 1;
+                } else {
+                    clearInterval(interval);
+                    const randomDomain = domainNames[Math.floor(Math.random() * domainNames.length)];
+                    setSelectedDomain(randomDomain);
+                    return null;
+                }
+            });
+        }, 2000);
+    };
+
+    return (
+        <div className='container'>
+            <div className="header">
+                <h1>HackWave 1.0</h1>
+                <h2>Sri Vasavi Engineering College - Department of CAI, AI & ML</h2>
+            </div>
+
+            <div className='domain-list'>
+                {(!selectedDomain && !countdown) ? (
+                    <>
+                        <h2>Domain List</h2>
+                        <ul>
+                            {domainNames.map((domain, index) => (
+                                <li key={index}>
+                                    <span>{domain}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </>
+                ) : (
+                    <div id="result" className='result'>
+                        {countdown !== null && (
+                            <div className="countdown-circle">{countdown}</div>
+                        )}
+                        {selectedDomain && (
+                            <h2 className="reveal-animation">{selectedDomain}</h2>
+                        )}
+                    </div>
+                )}
+            </div>
+
+            <p className='selector'>Click below to reveal your challenge domain...!</p>
+            <button onClick={selectRandomDomain} className='select-btn'>Reveal a Magical Domain</button>
+
+            <div className="powered-by">
+                <div>
+                    <p>Powered by</p>
+                    <img src={unStop} alt="Powered by Unstop" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DomainSelector;
